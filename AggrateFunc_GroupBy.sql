8aa9498f85c3afdcc01e62fe1965c8b30c8140bf
Tolgahan Bora <75574405+tolgahanbora@users.noreply.github.com>
2022-09-04 20:50:09 +0300

    Update AggrateFunc_GroupBy.sql

--- AggrateFunc_GroupBy.sql
+++ AggrateFunc_GroupBy.sql
@@ -9,3 +9,13 @@ SELECT AVG(rental_rate) FROM film ;
 SELECT COUNT(title) FROM film WHERE title LIKE 'C%';
 SELECT title,rental_rate,length FROM film WHERE  rental_rate = 0.99 ORDER BY length DESC;
 SELECT COUNT(DISTINCT replacement_cost) FROM film WHERE length > 150
+
+
+Group By and Having Operators:
+
+SELECT DISTINCT title, rating FROM film ORDER BY rating;
+SELECT title, COUNT(replacement_cost) FROM film 
+ORDER BY replacement_cost
+HAVING replacement_cost > 50;
+SELECT country_id,COUNT(city) FROM city GROUP BY country_id ORDER BY COUNT(city) DESC LIMIT 1
+
