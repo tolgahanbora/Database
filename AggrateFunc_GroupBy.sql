0937ca6c7954cb9781f8e889a59392bb6adb71b9
Tolgahan Bora <75574405+tolgahanbora@users.noreply.github.com>
2022-09-04 20:57:40 +0300

    Update AggrateFunc_GroupBy.sql

--- AggrateFunc_GroupBy.sql
+++ AggrateFunc_GroupBy.sql
@@ -14,8 +14,7 @@ SELECT COUNT(DISTINCT replacement_cost) FROM film WHERE length > 150
 Group By and Having Operators:
 
 SELECT DISTINCT title, rating FROM film ORDER BY rating;
-SELECT title, COUNT(replacement_cost) FROM film 
-ORDER BY replacement_cost
-HAVING replacement_cost > 50;
+SELECT COUNT(title) FROM film GROUP BY replacement_cost
+HAVING COUNT(replacement_cost) > 50  //HAVING ORDER BY ile kullanılmıyor, GROUP BY ile kullanılıyor ve SELECT sonrasında sadece COUNT ile kullanılması gererkiyor.
 SELECT country_id,COUNT(city) FROM city GROUP BY country_id ORDER BY COUNT(city) DESC LIMIT 1
 
