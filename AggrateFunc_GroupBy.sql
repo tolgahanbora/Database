34f2bec55d3461641487390e52c4ed15a7b94b54
Tolgahan Bora <75574405+tolgahanbora@users.noreply.github.com>
2022-09-04 19:11:03 +0300

    Update AggrateFunc_GroupBy.sql

--- AggrateFunc_GroupBy.sql
+++ AggrateFunc_GroupBy.sql
@@ -1,3 +1,11 @@
 Order By and LIMIT - OFFSET Operators:
 
 SELECT title FROM film WHERE title LIKE '%n' ORDER BY title,length DESC LIMIT 5 ;
+
+
+Aggregate Func and Order By:
+
+SELECT AVG(rental_rate) FROM film ;
+SELECT COUNT(title) FROM film WHERE title LIKE 'C%';
+SELECT title,rental_rate,length FROM film WHERE  rental_rate = 0.99 ORDER BY length DESC;
+SELECT COUNT(DISTINCT replacement_cost) FROM film WHERE length > 150
