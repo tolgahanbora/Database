51df7c11459c57ade38f2bfae94d8372712d1fbf
Tolgahan Bora <75574405+tolgahanbora@users.noreply.github.com>
2022-09-05 03:47:17 +0300

    Update JoinStructure.sql

--- JoinStructure.sql
+++ JoinStructure.sql
@@ -3,3 +3,9 @@ INNER JOIN STRUCTURE :
 SELECT * FROM film INNER JOIN film_actor ON film.film_id = film_actor.actor_id;
 SELECT city,country FROM city INNER JOIN country ON city.city_id = country.country_id;
 SELECT * FROM customer INNER JOIN payment ON  customer.customer_id = payment.payment_id
+
+
+LEFT JOIN and FULL JOIN:
+
+SELECT city,country FROM city LEFT JOIN country ON city.city_id = country.country_id;
+SELECT first_name, last_name FROM customer FULL JOIN rental ON rental.rental_id = customer.customer_id
